873b16f6afa285e5e8192b064c50858eba75ea4d
Građanin F <filipkr@protonmail.com>
2022-05-14 15:10:07 +0200

    Prvi commit

---
+++ hotel.sql
@@ -0,0 +1,42 @@
+# Hotel
+
+DROP DATABASE IF EXISTS hotel;
+CREATE DATABASE hotel default charset utf8mb4;
+USE hotel;
+
+CREATE TABLE soba(
+    sifra INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    broj INT NOT NULL,
+    broj_kreveta INT NOT NULL,
+    velicina_kreveta VARCHAR(10),
+    cijena_nocenja DECIMAL(18,2),
+    zauzeta BOOLEAN,
+    gost INT
+);
+
+CREATE TABLE gost(
+    sifra INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    ime VARCHAR(50) NOT NULL,
+    prezime VARCHAR(50) NOT NULL,
+    datum_rodenja DATETIME NOT NULL,
+    drzava VARCHAR(50) NOT NULL,
+    oib CHAR(11),
+    broj_putovnice VARCHAR(50),
+    soba INT NOT NULL
+);
+
+CREATE TABLE nocenje(
+    sifra INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    gost INT NOT NULL,
+    soba INT NOT NULL,
+    datum_prijave DATETIME NOT NULL,
+    datum_odjave DATETIME,
+    cijena_ukupno DECIMAL(18,2)
+);
+
+ALTER TABLE soba ADD FOREIGN KEY (gost) REFERENCES gost(sifra);
+
+ALTER TABLE gost ADD FOREIGN KEY (soba) REFERENCES soba(sifra);
+
+ALTER TABLE nocenje ADD FOREIGN KEY (gost) REFERENCES gost(sifra);
+ALTER TABLE nocenje ADD FOREIGN KEY (soba) REFERENCES soba(sifra);
